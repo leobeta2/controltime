13443b993629298fede567b2e3b100c4e79d602b
León Vidal Navarro <lvidal@optimisa.cl>
2017-01-25 17:15:29 -0300

    Se mejoro el diseño del Home, se muestra al usuario logueado

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,4 +1,5 @@
 var divMapa = document.getElementById('mapa');
+var divInfomapa = document.getElementById('infomapa');
 
   navigator.geolocation.getCurrentPosition(fn_ok, fn_mal);
   function fn_mal(){}
@@ -7,6 +8,6 @@ var divMapa = document.getElementById('mapa');
     var lon = respuesta.coords.longitude;
     var coordenada = lat+','+lon;
 
-    divMapa.innerHTML = '<img src="http://maps.googleapis.com/maps/api/staticmap?size=1200x800&markers='+coordenada+'"/>';
-
+    divMapa.innerHTML = '<img src="http://maps.googleapis.com/maps/api/staticmap?size=700x350&markers='+coordenada+'"/>';
+    divInfomapa.innerHTML=coordenada;
   }
